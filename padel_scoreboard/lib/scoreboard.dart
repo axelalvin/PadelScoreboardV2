25af99e65bfcbefdec86d80797e1314a53fd9941
axelalvin <axelalvin1996@gmail.com>
2022-04-26 10:13:52 +0200

    impl. point deuce, got started on game deuce

--- padel_scoreboard/lib/scoreboard.dart
+++ padel_scoreboard/lib/scoreboard.dart
@@ -12,12 +12,44 @@ class Scoreboard extends StatefulWidget {
 }
 
 class _ScoreboardState extends State<Scoreboard> {
-  int _counter = 0;
+  static const double columnHeight = 60.0;
+  static const double nameColumnWidth = 100.0;
+  static const double scoreColumnWidth = 37.0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  int max_sets = 5;
+  bool golden_point = false;
+
+  bool gameDeuce = false;
+  bool setDeuce = false;
+  bool tieBreak = false;
+  bool match_finished = false;
+
+  String team1 = 'ALV/PET';
+  String team2 = 'PET/ALV';
+
+  int currSet = 0;
+  List<int> team1GameCount = [];
+  List<int> team2GameCount = [];
+
+  int team1SetCount = 0;
+  int team2SetCount = 0;
+
+  List<int> scoreList = [0, 15, 30, 40, 41];
+  String team1CurrentGameScore = '0';
+  int team1ScorePos = 0;
+  String team2CurrentGameScore = '0';
+  int team2ScorePos = 0;
+
+  @override
+  void initState() {
+    team1CurrentGameScore = scoreList[team1ScorePos].toString();
+    team2CurrentGameScore = scoreList[team2ScorePos].toString();
+
+    for (int i = 0; i < max_sets; i++) {
+      team1GameCount.add(0);
+      team2GameCount.add(0);
+    }
+    super.initState();
   }
 
   @override
@@ -29,85 +61,301 @@ class _ScoreboardState extends State<Scoreboard> {
       body: Center(
         child: Column(
           children: <Widget>[
-            _addPadding(30),
+            _addVertPadding(15),
             Padding(
               padding: const EdgeInsets.fromLTRB(15, 0.0, 15, 0.0),
               child: Table(
                 border: TableBorder.all(),
-                columnWidths: const <int, TableColumnWidth>{
-                  0: IntrinsicColumnWidth(),
-                  1: FlexColumnWidth(),
-                  2: FixedColumnWidth(64),
-                },
+                columnWidths: _initColumns(max_sets),
                 defaultVerticalAlignment: TableCellVerticalAlignment.middle,
                 children: <TableRow>[
                   TableRow(
-                    children: <Widget>[
-                      Container(
-                        height: 32,
-                        color: Colors.green,
-                      ),
-                      TableCell(
-                        verticalAlignment: TableCellVerticalAlignment.top,
-                        child: Container(
-                          height: 32,
-                          width: 32,
-                          color: Colors.red,
-                        ),
-                      ),
-                      Container(
-                        height: 64,
-                        color: Colors.blue,
-                      ),
-                    ],
+                    children: _paintColumns(max_sets, team1, 1),
                   ),
                   TableRow(
-                    decoration: const BoxDecoration(
-                      color: Colors.grey,
-                    ),
-                    children: <Widget>[
-                      Container(
-                        height: 64,
-                        width: 128,
-                        color: Colors.purple,
-                      ),
-                      Container(
-                        height: 32,
-                        color: Colors.yellow,
-                      ),
-                      Center(
-                        child: Container(
-                          height: 32,
-                          width: 32,
-                          color: Colors.orange,
-                        ),
-                      ),
-                    ],
+                    children: _paintColumns(max_sets, team2, 2),
                   ),
                 ],
               ),
             ),
-            const Text(
-              'You have pushed the button this many times:',
+            _addVertPadding(30),
+            Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, //Center Row contents horizontally,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  team1,
+                ),
+                _addHorizPadding(35),
+                Text(
+                  team2,
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, //Center Row contents horizontally,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  //style: style,
+                  onPressed: () {
+                    if (gameDeuce)
+                      _addDeucePoint(1);
+                    else if (!match_finished && !gameDeuce) _addPoint(1);
+                  },
+                  child: const Text('Point'),
+                ),
+                _addHorizPadding(20),
+                ElevatedButton(
+                  //style: style,
+                  onPressed: () {
+                    if (gameDeuce)
+                      _addDeucePoint(2);
+                    else if (!match_finished && !gameDeuce) _addPoint(2);
+                  },
+                  child: const Text('Point'),
+                ),
+              ],
             ),
-            const Text('You are ugly'),
+            Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, //Center Row contents horizontally,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  //style: style,
+                  onPressed: () {
+                    if (setDeuce)
+                      _addDeuceGame(1);
+                    else if (!match_finished && !setDeuce) _addGame(1);
+                  },
+                  child: const Text('Game'),
+                ),
+                _addHorizPadding(20),
+                ElevatedButton(
+                  //style: style,
+                  onPressed: () {
+                    if (setDeuce)
+                      _addDeuceGame(2);
+                    else if (!match_finished && !setDeuce) _addGame(2);
+                  },
+                  child: const Text('Game'),
+                ),
+              ],
+            ),
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
-  Padding _addPadding(double height) {
+  void _addPoint(int team) {
+    setState(() {
+      if (team == 1) {
+        team1ScorePos++;
+        if (scoreList[team1ScorePos] == 41) {
+          //end of game
+          _addGame(team);
+        } else {
+          team1CurrentGameScore = scoreList[team1ScorePos].toString();
+        }
+      } else {
+        team2ScorePos++;
+        if (scoreList[team2ScorePos] == 41) {
+          //end of game
+          _addGame(team);
+        } else {
+          team2CurrentGameScore = scoreList[team2ScorePos].toString();
+        }
+      }
+      if (!golden_point) {
+        if (scoreList[team1ScorePos] == 40 && scoreList[team2ScorePos] == 40) {
+          gameDeuce = true;
+        }
+      }
+    });
+  }
+
+  void _addDeucePoint(int team) {
+    bool deuce = false, team1adv = false, team2adv = false;
+    String adv = 'Adv';
+    if (team1CurrentGameScore == '40' && team2CurrentGameScore == '40') {
+      deuce = true;
+    }
+    if (team1CurrentGameScore == adv) {
+      team1adv = true;
+    }
+    if (team2CurrentGameScore == adv) {
+      team2adv = true;
+    }
+    setState(() {
+      if (team == 1) {
+        if (deuce) {
+          team1CurrentGameScore = adv;
+          team2CurrentGameScore = '';
+        } else {
+          if (team1adv) {
+            _addGame(team);
+            gameDeuce = false;
+          } else {
+            team1CurrentGameScore = scoreList[team1ScorePos].toString();
+            team2CurrentGameScore = scoreList[team2ScorePos].toString();
+          }
+        }
+      } else {
+        if (deuce) {
+          team2CurrentGameScore = adv;
+          team1CurrentGameScore = '';
+        } else {
+          if (team2adv) {
+            _addGame(team);
+            gameDeuce = false;
+          } else {
+            team1CurrentGameScore = scoreList[team1ScorePos].toString();
+            team2CurrentGameScore = scoreList[team2ScorePos].toString();
+          }
+        }
+      }
+    });
+  }
+
+  void _resetPoints() {
+    team1ScorePos = 0;
+    team2ScorePos = 0;
+
+    setState(() {
+      team1CurrentGameScore = scoreList[team1ScorePos].toString();
+      team2CurrentGameScore = scoreList[team2ScorePos].toString();
+    });
+  }
+
+  void _addGame(int team) {
+    setState(() {
+      _resetPoints();
+      if (team == 1) {
+        team1GameCount[currSet]++;
+        if (team1GameCount[currSet] == 6) {
+          if (currSet < max_sets - 1) {
+            team1SetCount++;
+            currSet++;
+          } else {
+            team1SetCount++;
+            match_finished = true;
+          }
+        }
+      } else {
+        team2GameCount[currSet]++;
+        if (team2GameCount[currSet] == 6) {
+          if (currSet < max_sets - 1) {
+            team2SetCount++;
+            currSet++;
+          } else {
+            team2SetCount++;
+            match_finished = true;
+          }
+        }
+      }
+
+      if (team1GameCount[currSet] == 5 && team2GameCount[currSet] == 5) {
+        setDeuce = true;
+      }
+    });
+  }
+
+  void _addDeuceGame(int team) {
+    setState(() {
+      _resetPoints();
+      if (team == 1) {
+      } else {}
+    });
+  }
+
+  Map<int, TableColumnWidth>? _initColumns(int sets) {
+    if (sets == 3) {
+      return const <int, TableColumnWidth>{
+        0: FixedColumnWidth(nameColumnWidth),
+        1: FixedColumnWidth(scoreColumnWidth),
+        2: FixedColumnWidth(scoreColumnWidth),
+        3: FixedColumnWidth(scoreColumnWidth),
+        4: FixedColumnWidth(scoreColumnWidth),
+        5: FixedColumnWidth(scoreColumnWidth),
+        6: FixedColumnWidth(scoreColumnWidth),
+      };
+    }
+    if (sets == 5) {
+      return const <int, TableColumnWidth>{
+        0: FixedColumnWidth(nameColumnWidth),
+        1: FixedColumnWidth(scoreColumnWidth),
+        2: FixedColumnWidth(scoreColumnWidth),
+        3: FixedColumnWidth(scoreColumnWidth),
+        4: FixedColumnWidth(scoreColumnWidth),
+        5: FixedColumnWidth(scoreColumnWidth),
+        6: FixedColumnWidth(scoreColumnWidth),
+        7: FixedColumnWidth(scoreColumnWidth),
+        8: FixedColumnWidth(scoreColumnWidth),
+      };
+    }
+  }
+
+  List<Widget>? _paintColumns(int sets, String teamName, int team) {
+    List<Widget>? list = [];
+    list.add(_paintContainer(teamName, Colors.blue));
+
+    for (int i = 0; i < sets; i++) {
+      if (team == 1) {
+        list.add(_paintContainer(team1GameCount[i].toString(), Colors.green));
+      } else {
+        list.add(_paintContainer(team2GameCount[i].toString(), Colors.green));
+      }
+    }
+    if (team == 1) {
+      list.add(_paintContainer(team1SetCount.toString(), Colors.red));
+    } else {
+      list.add(_paintContainer(team2SetCount.toString(), Colors.red));
+    }
+    if (team == 1) {
+      list.add(_paintContainer(team1CurrentGameScore, Colors.yellow));
+    } else {
+      list.add(_paintContainer(team2CurrentGameScore, Colors.yellow));
+    }
+
+    return list;
+  }
+
+  Container _paintContainer(String text, Color? color) {
+    return Container(
+      height: columnHeight,
+      color: color,
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(
+          text,
+          style: TextStyle(),
+        ),
+      ),
+    );
+  }
+
+  Padding _addVertPadding(double height) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
       child: Container(
         padding: const EdgeInsets.all(0.0),
         width: 80.0,
         height: height,
+      ), //Container
+    );
+  }
+
+  Padding _addHorizPadding(double width) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+      child: Container(
+        padding: const EdgeInsets.all(0.0),
+        width: width,
+        height: 10.0,
       ), //Container
     );
   }
